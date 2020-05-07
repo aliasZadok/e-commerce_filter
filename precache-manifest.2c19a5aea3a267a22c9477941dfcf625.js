6138a1b923eaac9616bd69f918d16b246e5230e7
aliasZadok <lfstone3@gmail.com>
2020-05-07 17:39:08 +0530

    Updates

---
+++ precache-manifest.2c19a5aea3a267a22c9477941dfcf625.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0edf023d3eca67703cc8a10fb3b83904",
+    "url": "/e-commerce_filter/index.html"
+  },
+  {
+    "revision": "9ff4febc3a6f4eea96ae",
+    "url": "/e-commerce_filter/static/js/2.ae3e3646.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/e-commerce_filter/static/js/2.ae3e3646.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4dba67915cb117496c9",
+    "url": "/e-commerce_filter/static/js/main.40a0b759.chunk.js"
+  },
+  {
+    "revision": "5f2f793fffe6f5698ec6",
+    "url": "/e-commerce_filter/static/js/runtime-main.bb8f0168.js"
+  }
+]);
